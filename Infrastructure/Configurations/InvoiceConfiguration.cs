3110077436a45244c8ec6529aefd2254ee8ef9ed
pustmridul <pust.mridul@gmail.com>
2024-07-25 11:55:06 +0600

    no message

---
+++ Infrastructure/Configurations/InvoiceConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entity;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Configurations;
+
+
+public class InvoiceConfiguration : IEntityTypeConfiguration<Invoice>
+{
+    public void Configure(EntityTypeBuilder<Invoice> builder)
+    {
+        builder.ToTable("Invoice");
+        builder.HasKey(e => new { e.Id });
+        builder.Property(e => e.Id).ValueGeneratedOnAdd();
+    }
+}
