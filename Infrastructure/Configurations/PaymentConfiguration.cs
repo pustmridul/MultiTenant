2615a68387255ceeb792ac778a978899cb574822
Mehrab-HossainCSE <mehrab15-13769@diu.edu.bd>
2024-07-27 10:43:41 +0600

    paymentConfiguration is added

---
+++ Infrastructure/Configurations/PaymentConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entity;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Configurations;
+
+public class PaymentConfiguration: IEntityTypeConfiguration<Payment>
+{
+    public void Configure(EntityTypeBuilder<Invoice> builder)
+    {
+        builder.ToTable("Invoice");
+        builder.HasKey(e => new { e.Id });
+        builder.Property(e => e.Id).ValueGeneratedOnAdd();
+    }
+}
