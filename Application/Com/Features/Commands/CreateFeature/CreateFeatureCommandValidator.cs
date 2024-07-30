fa73f518ad1dbb006f326116e2134da8ca49cc2a
Mehrab-HossainCSE <mehrab15-13769@diu.edu.bd>
2024-07-30 15:21:47 +0600

    controller added

---
+++ Application/Com/Features/Commands/CreateFeature/CreateFeatureCommandValidator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Com.Features.Commands.CreateFeature
+{
+    internal class CreateFeatureCommandValidator : AbstractValidator<CreateFeatureCommand>
+    {
+        public CreateFeatureCommandValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Name is required.")
+                .MaximumLength(100).WithMessage("Name must not exceed 100 characters.");
+
+            RuleFor(x => x.Description)
+                    .MaximumLength(500).WithMessage("Description must not exceed 500 characters.");
+        }
+    }
+}
+
