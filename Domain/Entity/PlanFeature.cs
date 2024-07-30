b0a021c01efce002c5042bfb21d0d5e9a57a178b
pustmridul <pust.mridul@gmail.com>
2024-07-30 13:00:18 +0600

    no message

---
+++ Domain/Entity/PlanFeature.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entity
+{
+    public class PlanFeature : BaseEntity
+    {
+       public int PlanId {  get; set; }
+       public int FeatureId { get; set; }
+    }
+}
---
+++ Domain/Entity/Pricing.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entity
+{
+    public class Pricing : BaseEntity
+    {
+        public int PlanId { get; set; }
+        public decimal Price { get; set; }
+    }
+}
