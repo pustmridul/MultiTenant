9f9eee2424901e722fc6f29f499b4834923a257b
pustmridul <pust.mridul@gmail.com>
2024-08-11 14:11:25 +0600

    no

--- Infrastructure/Configurations/PermissionConfiguration.cs
+++ Infrastructure/Configurations/PermissionConfiguration.cs
@@ -13,13 +13,10 @@ internal class PermissionConfiguration: IEntityTypeConfiguration<Permission>
         builder.HasData(
                new Permission { Id = 1, PermissionCode = "User.View", PermissionText = "User", Created = DateTime.UtcNow, Updated = DateTime.UtcNow },
                new Permission { Id = 2, PermissionCode = "User.Create", PermissionText = "User", Created = DateTime.UtcNow, Updated = DateTime.UtcNow },
-               new Permission { Id = 3, PermissionCode = "User.Remove", PermissionText = "User", Created = DateTime.UtcNow, Updated = DateTime.UtcNow },
-               new Permission { Id = 4, PermissionCode = "Tenant.View", PermissionText = "Tenant", Created = DateTime.UtcNow, Updated = DateTime.UtcNow },
-               new Permission { Id = 5, PermissionCode = "Tenant.Create", PermissionText = "Tenant", Created = DateTime.UtcNow, Updated = DateTime.UtcNow },
-               new Permission { Id = 6, PermissionCode = "Tenant.Remove", PermissionText = "Tenant", Created = DateTime.UtcNow, Updated = DateTime.UtcNow },
-               new Permission { Id = 7, PermissionCode = "Role.View", PermissionText = "Role", Created = DateTime.UtcNow, Updated = DateTime.UtcNow },
-               new Permission { Id = 8, PermissionCode = "Role.Create", PermissionText = "Role", Created = DateTime.UtcNow, Updated = DateTime.UtcNow },
-               new Permission { Id = 9, PermissionCode = "Role.Remove", PermissionText = "Role", Created = DateTime.UtcNow, Updated = DateTime.UtcNow });
+               new Permission { Id = 3, PermissionCode = "User.Remove", PermissionText = "User", Created = DateTime.UtcNow, Updated = DateTime.UtcNow }
+           // Add more role-permission relationships as needed
+           );
+
 
     }
 }
